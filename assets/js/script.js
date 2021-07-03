b4f04d535361cffdd3c860cb35d4ed4f9e4f5f1f
TheHebi <nturcotte8@live.com>
2021-07-03 15:00:37 -0700

    fixed issue with dynamically generated buttons not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,12 @@
 
 // gloabalvars
 var apiKey = `89ccc6e1a0a469bc77ebd2c54993b60a`;
-var history = [];
+const history = [];
 
 function getWeather(event) {
-  event.preventDefault();
   var cityName = $("#inputCity").val();
+  history.push(`${cityName}`);
+  localStorage.setItem("cities", JSON.stringify(history));
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
   )
@@ -24,7 +25,7 @@ function getWeather(event) {
         "src",
         `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
       );
-      $("#currentIcon").removeClass("noDisplay")
+      $("#currentIcon").removeClass("noDisplay");
       $("#currentTemp").text(`Temp: ${data.main.temp} F`);
       $("#currentWind").text(`Wind ${data.wind.speed} MPH`);
       $("#currentHumid").text(`Humidity: ${data.main.humidity}%`);
@@ -52,29 +53,131 @@ function getWeather(event) {
             $("#currentUv").attr("class", "badge badge-danger");
           }
           for (let i = 1; i < 6; i++) {
-              $(`#icon${[i]}`).attr("src",`http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`);
-              $(`#icon${[i]}`).removeClass("noDisplay")
-              $(`#temp${[i]}`).text(`Temp: ${data.daily[i].temp.day} F`)
-              $(`#wind${[i]}`).text(`Wind: ${data.daily[i].wind_speed} MPH`)
-              $(`#humid${[i]}`).text(`Humidity: ${data.daily[i].humidity}%`)
-              $(`#uv${[i]}`).text(`UV Index: ${data.daily[i].uvi}`)
-              $(`#day${[i]}`).text(moment().add(1,"day").format("MM/DD/YYYY"))
-              if (data.daily[i].uvi < 4) {
-                  $(`#uv${[i]}`).attr("class", "badge badge-success");
-                } else if (data.daily[i].uvi < 8) {
-                    $(`#uv${[i]}`).attr("class", "badge badge-warning");
-                } else {
-                    $(`#uv${[i]}`).attr("class", "badge badge-danger");
-                }
-                
+            $(`#icon${[i]}`).attr(
+              "src",
+              `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`
+            );
+            $(`#icon${[i]}`).removeClass("noDisplay");
+            $(`#temp${[i]}`).text(`Temp: ${data.daily[i].temp.day} F`);
+            $(`#wind${[i]}`).text(`Wind: ${data.daily[i].wind_speed} MPH`);
+            $(`#humid${[i]}`).text(`Humidity: ${data.daily[i].humidity}%`);
+            $(`#uv${[i]}`).text(`UV Index: ${data.daily[i].uvi}`);
+            $(`#day${[i]}`).text(moment().add([i], "day").format("MM/DD/YYYY"));
+            if (data.daily[i].uvi < 4) {
+              $(`#uv${[i]}`).attr("class", "badge badge-success");
+            } else if (data.daily[i].uvi < 8) {
+              $(`#uv${[i]}`).attr("class", "badge badge-warning");
+            } else {
+              $(`#uv${[i]}`).attr("class", "badge badge-danger");
+            }
           }
         });
+      $("#inputCity").val("");
     });
-    // appends previous search to page TODO: keep on page post refresh
-  var oldCity = $("<button>");
-  $(oldCity).attr("class", "btn btn-secondary w-100 oldCitybtn");
-  $(oldCity).text(`${cityName}`);
-  $(".formBox").append(oldCity);
-  $("#inputCity").val("");
+//   appends previous search to page TODO: keep on page post refresh
+    var oldCity = $("<button>");
+    $(oldCity).attr("class", "btn btn-secondary w-100 oldCityBtn");
+    $(oldCity).text(`${cityName}`);
+    $(oldCity).attr("type", "button");
+    $(oldCity).attr("cursor", "pointer");
+    $(oldCity).attr("cities", `${cityName}`)
+    $(".formBox").append(oldCity);
 }
+
+function saveCity(){
+
+    for (let i = 0; i < history.length; i++) {
+        var oldCity = $("<button>");
+        $(oldCity).attr("class", "btn btn-secondary w-100 oldCityBtn");
+        $(oldCity).text(history[i]);
+        $(oldCity).attr("type", "button");
+        $(oldCity).attr("cursor", "pointer");
+        $(oldCity).attr("cities", history[i])
+        $(".formBox").append(oldCity);
+    }
+        
+}
+
+window.onload = function() {
+    var reloading = sessionStorage.getItem("reloading");
+    if (reloading) {
+        sessionStorage.removeItem("reloading");
+        saveCity();
+    }
+}
+
+function reloadP() {
+    sessionStorage.setItem("reloading", "true");
+    document.location.reload();
+}
+
+var pastWeather = function(event){
+    var prevCity = $(event.target).text();
+    console.log(prevCity)
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${prevCity}&appid=${apiKey}&units=imperial`
+  )
+    .then(function (data) {
+      return data.json();
+    })
+    // current weather forecast
+    .then(function (data) {
+      console.log(data);
+      $("#cityName").text(`${data.name} ${moment().format("MM/DD/YYYY")}`);
+      $("#currentIcon").attr(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+      );
+      $("#currentIcon").removeClass("noDisplay");
+      $("#currentTemp").text(`Temp: ${data.main.temp} F`);
+      $("#currentWind").text(`Wind ${data.wind.speed} MPH`);
+      $("#currentHumid").text(`Humidity: ${data.main.humidity}%`);
+      document
+        .getElementById("currentBox")
+        .setAttribute("class", "border border-dark");
+      // get uv index
+      var long = data.coord.lon;
+      var lat = data.coord.lat;
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=imperial`
+      )
+        .then(function (data) {
+          return data.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          console.log(data.daily[0].uvi);
+          $("#currentUv").text(`UV Index: ${data.daily[0].uvi}`);
+          if (data.daily[0].uvi < 4) {
+            $("#currentUv").attr("class", "badge badge-success");
+          } else if (data.daily[0].uvi < 8) {
+            $("#currentUv").attr("class", "badge badge-warning");
+          } else {
+            $("#currentUv").attr("class", "badge badge-danger");
+          }
+          for (let i = 1; i < 6; i++) {
+            $(`#icon${[i]}`).attr(
+              "src",
+              `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`
+            );
+            $(`#icon${[i]}`).removeClass("noDisplay");
+            $(`#temp${[i]}`).text(`Temp: ${data.daily[i].temp.day} F`);
+            $(`#wind${[i]}`).text(`Wind: ${data.daily[i].wind_speed} MPH`);
+            $(`#humid${[i]}`).text(`Humidity: ${data.daily[i].humidity}%`);
+            $(`#uv${[i]}`).text(`UV Index: ${data.daily[i].uvi}`);
+            $(`#day${[i]}`).text(moment().add([i], "day").format("MM/DD/YYYY"));
+            if (data.daily[i].uvi < 4) {
+              $(`#uv${[i]}`).attr("class", "badge badge-success");
+            } else if (data.daily[i].uvi < 8) {
+              $(`#uv${[i]}`).attr("class", "badge badge-warning");
+            } else {
+              $(`#uv${[i]}`).attr("class", "badge badge-danger");
+            }
+          }
+        });
+      $("#inputCity").val("");
+    });
+}
+
 $("#submitBtn").on("click", getWeather);
+$(document).on("click", ".oldCityBtn", pastWeather);
